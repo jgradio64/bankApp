7b565ac5e77c30a98cccbdbded2f6a173b921e4c
Eich <benjamin.eich@autovin.com>
2022-06-13 17:24:20 -0400

    Refactor some unit tests

--- BankTests/BankTests.cs
+++ BankTests/BankTests.cs
@@ -14,7 +14,7 @@ namespace BankTests
         [TestMethod]
         public void BankShouldHaveNoAccounts()
         {
-            Bank NewBank = new Bank("000001", "Some Bank Name");
+            Bank NewBank = new("000001", "Some Bank Name");
             List<Account> actual = NewBank.GetAccountList();
             Assert.AreEqual(0, actual.Count);
         }
@@ -22,9 +22,9 @@ namespace BankTests
         [TestMethod]
         public void InitialDepositShouldWork()
         {
-            Bank NewBank = new Bank("000001", "Some Bank Name");
-            IndividualOwner NewOwner = new IndividualOwner("1", "Itch", "Skill", "iskill0@irs.gov", "718-632-9460", "1 Fairview Trail", "Brooklyn", "New York", "11225");
-            IndividualInvevtmentAccount NewAccount = new IndividualInvevtmentAccount(NewOwner, "1", 400);
+            Bank NewBank = new("000001", "Some Bank Name");
+            IndividualOwner NewOwner = new("1", "Itch", "Skill", "iskill0@irs.gov", "718-632-9460", "1 Fairview Trail", "Brooklyn", "New York", "11225");
+            IndividualInvevtmentAccount NewAccount = new(NewOwner, "1", 400);
             NewBank.AddAccount(NewAccount);
             Double accountBalance = NewBank.FindAccountByID("1").GetBalance();
             Assert.AreEqual(400, accountBalance);
@@ -35,9 +35,9 @@ namespace BankTests
         [TestMethod]
         public void AdditionalDepositShouldWork()
         {
-            Bank NewBank = new Bank("000001", "Some Bank Name");
-            IndividualOwner NewOwner = new IndividualOwner("1", "Itch", "Skill", "iskill0@irs.gov", "718-632-9460", "1 Fairview Trail", "Brooklyn", "New York", "11225");
-            IndividualInvevtmentAccount NewAccount = new IndividualInvevtmentAccount(NewOwner, "1", 400);
+            Bank NewBank = new("000001", "Some Bank Name");
+            IndividualOwner NewOwner = new("1", "Itch", "Skill", "iskill0@irs.gov", "718-632-9460", "1 Fairview Trail", "Brooklyn", "New York", "11225");
+            IndividualInvevtmentAccount NewAccount = new(NewOwner, "1", 400);
             NewBank.AddAccount(NewAccount);
             NewBank.FindAccountByID("1").Deposit(500);
             Double accountBalance = NewBank.FindAccountByID("1").GetBalance();
@@ -47,9 +47,9 @@ namespace BankTests
         [TestMethod]
         public void WithdrawShouldWork()
         {
-            Bank NewBank = new Bank("000001", "Some Bank Name");
-            IndividualOwner NewOwner = new IndividualOwner("1", "Itch", "Skill", "iskill0@irs.gov", "718-632-9460", "1 Fairview Trail", "Brooklyn", "New York", "11225");
-            IndividualInvevtmentAccount NewAccount = new IndividualInvevtmentAccount(NewOwner, "1", 400);
+            Bank NewBank = new("000001", "Some Bank Name");
+            IndividualOwner NewOwner = new("1", "Itch", "Skill", "iskill0@irs.gov", "718-632-9460", "1 Fairview Trail", "Brooklyn", "New York", "11225");
+            IndividualInvevtmentAccount NewAccount = new(NewOwner, "1", 400);
             NewBank.AddAccount(NewAccount);
             NewBank.FindAccountByID("1").Withdraw(200);
             Double accountBalance = NewBank.FindAccountByID("1").GetBalance();
@@ -59,9 +59,9 @@ namespace BankTests
         [TestMethod]
         public void WithdrawShouldWorkCoprorate()
         {
-            Bank NewBank = new Bank("000001", "Some Bank Name");
-            CorporateOwner NewOwner = new CorporateOwner("1", "I Skill Up Academy", "iskill0@irs.gov", "718-632-9460", "1 Fairview Trail", "Brooklyn", "New York", "11225");
-            CorporateInvestmentAccount NewAccount = new CorporateInvestmentAccount(NewOwner, "1", 4000);
+            Bank NewBank = new("000001", "Some Bank Name");
+            CorporateOwner NewOwner = new("1", "I Skill Up Academy", "iskill0@irs.gov", "718-632-9460", "1 Fairview Trail", "Brooklyn", "New York", "11225");
+            CorporateInvestmentAccount NewAccount = new(NewOwner, "1", 4000);
             NewBank.AddAccount(NewAccount);
             NewBank.FindAccountByID("1").Withdraw(2000);
             Double accountBalance = NewBank.FindAccountByID("1").GetBalance();
@@ -72,9 +72,9 @@ namespace BankTests
         [ExpectedException(typeof(OverLimitException))]
         public void WithdrawShouldFail()
         {
-            Bank NewBank = new Bank("000001", "Some Bank Name");
-            IndividualOwner NewOwner = new IndividualOwner("1", "Itch", "Skill", "iskill0@irs.gov", "718-632-9460", "1 Fairview Trail", "Brooklyn", "New York", "11225");
-            IndividualInvevtmentAccount NewAccount = new IndividualInvevtmentAccount(NewOwner, "1", 800);
+            Bank NewBank = new("000001", "Some Bank Name");
+            IndividualOwner NewOwner = new("1", "Itch", "Skill", "iskill0@irs.gov", "718-632-9460", "1 Fairview Trail", "Brooklyn", "New York", "11225");
+            IndividualInvevtmentAccount NewAccount = new(NewOwner, "1", 800);
             NewBank.AddAccount(NewAccount);
             NewBank.FindAccountByID("1").Withdraw(600);
         }
@@ -82,11 +82,11 @@ namespace BankTests
         [TestMethod]
         public void TransferShouldFailDueToLackOfFunds()
         {
-            Bank NewBank = new Bank("000001", "Some Bank Name");
-            IndividualOwner IOwner1 = new IndividualOwner("1", "Itch", "Skill", "iskill0@irs.gov", "718-632-9460", "1 Fairview Trail", "Brooklyn", "New York", "11225");
-            IndividualOwner IOwner2 = new IndividualOwner("2", "Sigvard", "Doxey", "sdoxey1@unicef.org", "315-152-3050", "284 Redwing Circle", "Syracuse", "New York", "13205");
-            IndividualInvevtmentAccount InvAccount1 = new IndividualInvevtmentAccount(IOwner1, "1", 400);
-            IndividualInvevtmentAccount InvAccount2 = new IndividualInvevtmentAccount(IOwner2, "2", 0);
+            Bank NewBank = new("000001", "Some Bank Name");
+            IndividualOwner IOwner1 = new("1", "Itch", "Skill", "iskill0@irs.gov", "718-632-9460", "1 Fairview Trail", "Brooklyn", "New York", "11225");
+            IndividualOwner IOwner2 = new("2", "Sigvard", "Doxey", "sdoxey1@unicef.org", "315-152-3050", "284 Redwing Circle", "Syracuse", "New York", "13205");
+            IndividualInvevtmentAccount InvAccount1 = new(IOwner1, "1", 400);
+            IndividualInvevtmentAccount InvAccount2 = new(IOwner2, "2", 0);
             NewBank.AddAccount(InvAccount2);
             NewBank.AddAccount(InvAccount1);
             NewBank.TransferFundsBetweenAccounts("1", "2", 500);
@@ -99,11 +99,11 @@ namespace BankTests
         [TestMethod]
         public void TransferFundsShouldWork()
         {
-            Bank NewBank = new Bank("000001", "Some Bank Name");
-            IndividualOwner IOwner1 = new IndividualOwner("1", "Itch", "Skill", "iskill0@irs.gov", "718-632-9460", "1 Fairview Trail", "Brooklyn", "New York", "11225");
-            IndividualOwner IOwner2 = new IndividualOwner("2", "Sigvard", "Doxey", "sdoxey1@unicef.org", "315-152-3050", "284 Redwing Circle", "Syracuse", "New York", "13205");
-            IndividualInvevtmentAccount InvAccount1 = new IndividualInvevtmentAccount(IOwner1, "1", 600);
-            IndividualInvevtmentAccount InvAccount2 = new IndividualInvevtmentAccount(IOwner2, "2", 0);
+            Bank NewBank = new("000001", "Some Bank Name");
+            IndividualOwner IOwner1 = new("1", "Itch", "Skill", "iskill0@irs.gov", "718-632-9460", "1 Fairview Trail", "Brooklyn", "New York", "11225");
+            IndividualOwner IOwner2 = new("2", "Sigvard", "Doxey", "sdoxey1@unicef.org", "315-152-3050", "284 Redwing Circle", "Syracuse", "New York", "13205");
+            IndividualInvevtmentAccount InvAccount1 = new(IOwner1, "1", 600);
+            IndividualInvevtmentAccount InvAccount2 = new(IOwner2, "2", 0);
             NewBank.AddAccount(InvAccount2);
             NewBank.AddAccount(InvAccount1);
             NewBank.TransferFundsBetweenAccounts("1", "2", 500);
